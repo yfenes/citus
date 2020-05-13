a3f1361b4ba9a1304f77b47b6deff30a2cb3d659
Hanefi Önaldı <Hanefi.Onaldi@Microsoft.com>
2020-05-13 12:53:14 +0300

    Improve regexes

--- ci/disallow_c_comments_in_migrations.sh
+++ ci/disallow_c_comments_in_migrations.sh
@@ -5,4 +5,5 @@ set -eu
 # We do not use c-style comments in migration files as the stripped
 # zero-length migration files cause warning during packaging
 # See #3115 for more info
-find src/backend/distributed/sql/*.sql -print0 | xargs -0 sed -i 's#/\*\(.*\)\w*\*/#--\1#g'
+find src/backend/distributed/sql/*.sql -print0 | xargs -0 sed -i 's#^\s*/\*\(.*\)\w*\*/#--\1#g'
+find src/backend/distributed/sql/*.sql -print0 | xargs -0 sed -i 's#^\s*\*\/\s*##g'
